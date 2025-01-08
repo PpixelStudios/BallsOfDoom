63550ba9d9bcc1231f7f5be0c0f283423af54505
GabrielH4nma <mendesgabriel444@gmail.com>
2025-01-08 14:46:52 +0000

    Fim

--- BallsOfDoomNew/Assets/Scripts/FinishLine.cs
+++ BallsOfDoomNew/Assets/Scripts/FinishLine.cs
@@ -3,26 +3,29 @@ using UnityEngine;
 public class FinishLine : MonoBehaviour
 {
     private bool raceFinished = false;
-
+    private int playerCount = 0;
     void OnTriggerEnter(Collider other)
     {
         if (!raceFinished && other.CompareTag("Player"))
         {
-            raceFinished = true;
-            // Determina o índice da bola a partir do nome do objeto.
-            //int ballIndex = GetBallIndex(other.gameObject.name);
+            playerCount++;
 
-            // Atualiza a pontuação da bola vencedora.
-            //GameManager.Instance.AddScore(ballIndex, 10);
-
-            // Transição para a próxima sala (nome da próxima cena).
-            GameManager.Instance.LoadNextLevel("W");
-
-            Debug.Log($"{other.name} ganhou a corrida!");
+            if (playerCount == 1)
+            { // Primeiro jogador vai para a cena "W".
+                GameManager.Instance.LoadNextLevel("W");
+                Debug.Log($"{other.name} ganhou a corrida e foi para a cena W!");
+            }
+            else if (playerCount == 2)
+            { // Segundo jogador vai para a cena "L".
+                GameManager.Instance.LoadNextLevel("L");
+                Debug.Log($"{other.name} chegou em segundo e foi para a cena L!");
+                raceFinished = true;
+            }
         }
     }
 
-    private int GetBallIndex(string ballName)
+
+ private int GetBallIndex(string ballName)
     {
         // Assuma que os nomes das bolas são "Ball1", "Ball2", etc.
         if (ballName.StartsWith("Ball"))
